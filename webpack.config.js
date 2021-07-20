7fb78401dcdcbf9ac345225b5c6077d2182eaae8
Cielo <houng1004@gmail.com>
2021-07-20 11:43:48 +0800

    移除node_modules 調整圖片尺寸 透過webpack打包javascript

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'main.bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+			{
+				test: /.css$/, 
+				use: [
+					'style-loader',  
+					'css-loader'
+				]
+			}
+		]
+    }
+};
